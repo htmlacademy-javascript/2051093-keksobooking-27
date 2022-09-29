a8253d2ba67be551ccef33048f6cb6d296b64796
lazy_slipper <avpontriagina@gmail.com>
2022-09-30 02:11:31 +0300

    Fix errors

--- js/main.js
+++ js/main.js
@@ -6,9 +6,13 @@ function getRandomNumber (min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
+//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 function getRandomFraction (min, max) {
   if (min < 0, max < 0 ) {
     return NaN;
   }
   return Math.random() * (max - min) + min;
 }
+
+getRandomNumber(3,10);
+getRandomFraction (1.1, 5.4);
