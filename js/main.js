64e4b2dbaf91690b000f7f8d5fe198665077ccf9
lazy_slipper <avpontriagina@gmail.com>
2022-09-30 13:08:31 +0300

    Fix functions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
-function getRandomNumber (min, max) {
-  if (min < 0, max < 0 ) {
-    return NaN;
+const getRandomNumber = (min, max) => {
+  if (min < 0 || max < 0 ) {
+    return -1;
   }
   return Math.round(Math.random() * (max - min) + min);
-}
+};
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-function getRandomFraction (min, max) {
-  if (min < 0, max < 0 ) {
-    return NaN;
+const getRandomFraction = (min, max) => {
+  if (min < 0 || max < 0 ) {
+    return -1;
   }
   return Math.random() * (max - min) + min;
-}
+};
 
 getRandomNumber(3,10);
 getRandomFraction (1.1, 5.4);
+
+// console.log(getRandomFraction(10,26).toFixed(4));
